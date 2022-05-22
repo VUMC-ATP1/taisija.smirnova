040c08b20acdf0ee0f6d3e29036e62b6892c5644
taisijasmirnova <tasjuha94@gmail.com>
2022-05-22 14:13:19 +0300

    [feature/java]: homework

---
+++ src/main/java/homework/FirstClass.java
@@ -0,0 +1,64 @@
+package homework;
+
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        //Valsts
+        String CountryName = "Spain";
+        System.out.println(CountryName);
+
+        //Iedzivotaju skaits
+
+        int countryPopulation = 47265321;
+        System.out.printf("Country Population %d people\n", countryPopulation);
+
+        //Galvaspilseta
+        String CountryCapital = "Madrid";
+        System.out.println(CountryCapital);
+
+        //platiba
+
+        long countryArea = 5059090;
+        System.out.printf("Country Area: %d\n", countryArea);
+
+        //valoda
+        String CountryLanguage = "Spanish";
+        System.out.println(CountryLanguage);
+
+        //Vai ir ES
+        boolean CountryInEuropeanUnion = true;
+        System.out.println(CountryInEuropeanUnion);
+
+        //valuta
+        char CountryCurrency = '€';
+        System.out.println(CountryCurrency);
+
+        //3 piemeri
+
+        int a = 55;
+        int b = 8;
+        System.out.println(a + b);
+
+
+        int c = 100;
+        int d = 76;
+        System.out.println(c - d);
+
+
+        int l = 8;
+        int t = 3;
+        System.out.println(l * t);
+
+
+        int x = 42;
+        int y = x;
+        y = 7;
+        System.out.println(x); // cik ir "x"?
+
+
+
+    }
+
+
+}
